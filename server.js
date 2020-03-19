1008b6075f8b5c6d83e58852cabf2b5e429e314a
Robert Hall <rob@csart.space>
2020-03-19 23:12:12 +0000

    init NPM Packages

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log("HELLO WORLD");
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(3000);
+var io = socket(server);
+
+var userArray =[];
+
+app.use(express.static('../RingOfFireOMP-Client'));
+console.log("Listening on 3000");
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log('New Connection ' + socket.id);
+    
+    
+    socket.on('userConfirmed', function(data){
+        console.log("NewUserConfirmation");
+        userArray.push(data);
+        for(var i =0; i<userArray.length;i++){
+            console.log(userArray[i].nickname);
+        }
+        socket.emit('usersUpdate',userArray);
+    });
+
+    socket.on('TEST', (data)=>{
+        console.log('TEST'+ data);
+    });
+    
+};
+
