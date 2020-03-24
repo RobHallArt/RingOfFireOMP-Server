00509ab975de33b99f19f7002dcc02da30b99d4c
Robert Hall <rob@csart.space>
2020-03-24 16:38:52 +0000

    Turn Taking Works

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ var os = require( 'os' );
 
 var networkInterfaces = os.networkInterfaces( );
 
+var currentTurn = 0;
+var previousTurn = 0;
 
+var programState = 'Lobby';
 
 var userArray =[];
 
@@ -36,24 +39,36 @@ function newConnection(socket){
     });
 
     socket.on('userUpdate', function(data){
-        //console.log("userUpdate");
-        //search through things and check who updated and update them.
         for(var i = 0; i<userArray.length; i++){
-            //console.log(data.ID,userArray[i].ID);
             if(data.ID == userArray[i].ID){
-                console.log(data.nickname," Updated + isReady? : ",data.isReady);
                 userArray[i] = data;
             }
         }
-        //for(var i = 0; i<userArray.length;i++){
-        //    console.log(userArray[i].nickname);
-        //}
+        
         io.sockets.emit('usersUpdate',userArray);
 
+        var allReady = true;
+        for(var i = 0; i<userArray.length;i++){
+            if(allReady){
+                allReady = userArray[i].isReady;
+            }
+        }
+        console.log("Are We Ready? : ", allReady);
+        if(allReady){
+            io.sockets.emit('programStateUpdate', 'Game'); console.log("Going to game Mode");
+            nextTurn();
+            // at some point actually change programMode to game and do 
+        }
     });
 
     socket.on('TEST', (data)=>{
         console.log('TEST'+ data);
+    });
+
+    socket.on('turnFinished', (data)=>{
+        userArray[previousTurn].isActive = false;
+        io.sockets.emit('usersUpdate',userArray);
+        nextTurn();
     });
 
     socket.on('disconnect',(reason)=>{
@@ -68,4 +83,16 @@ function newConnection(socket){
     });
     
 };
+
+function nextTurn(){
+    for(var i = 0; i<userArray.length;i++){
+        userArray[i].isReady = false;           // not sure if necessary just yet.
+    }
+    userArray[currentTurn].isActive = true;
+    io.sockets.emit('usersUpdate',userArray);
+    io.to(`${userArray[currentTurn].ID}`).emit('yourTurn',userArray[currentTurn].nickname);
+    previousTurn = currentTurn;
+    currentTurn = (currentTurn+1)%userArray.length;
+
+}
 
